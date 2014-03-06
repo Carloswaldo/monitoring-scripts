cbc3d3f243ab269339ce62a2c8ea33fd413777bf
Carlos Chapi <carlos.chapi@2ndquadrant.com>
2014-03-06 17:49:26 -0500

    Adding check_repmgrd.sh

---
+++ check_repmgrd.sh
@@ -0,0 +1,8 @@
+PID=`pidof repmgrd`
+if test $? -ne 0; then
+        echo "CHECK_REPMGRD CRITICAL: pid not found"
+        exit 2
+else
+        echo "CHECK_REPMGRD OK: pid $PID"
+        exit 0
+fi
